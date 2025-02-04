511fbd8f5eb727135758d2da381b3b52af22113f
robertomattus <robertmatt113@gmail.com>
2025-02-04 15:39:33 -0600

    add sign in, sign up layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <div className="center">
-      <h1 className="text-3xl font-bold text-brand-100">
-        StoreIt - The only storage solution you need
-      </h1>
+    <div className="flex-center h-screen">
+      <h1 className="h1">StoreIt - The only storage solution you need</h1>
     </div>
   );
 }
