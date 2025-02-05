bb8bcac7d18a8a602a6fa24afa060050d2f39867
robertomattus <robertmatt113@gmail.com>
2025-02-04 18:26:20 -0600

    create login components and add appwrite config

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,5 @@
-import React from "react";
+import AuthForm from "@/components/AuthForm";
 
-const SignUp = () => {
-  return <div>Sign Up</div>;
-};
+const SignUp = () => <AuthForm type="sign-up" />;
 
 export default SignUp;
